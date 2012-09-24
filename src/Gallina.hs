3b304a0ff171466e715a9aaa94c98a8209932898
Gabe Dijkstra <gabe.dijkstra@gmail.com>
2012-09-24 16:12:26 +0200

    Type signatures can now be associated with the appropriate definition.

--- src/Gallina.hs
+++ src/Gallina.hs
@@ -20,10 +20,11 @@ data GallinaConstructor = GallinaConstructor { constrName :: String
                                              , fields :: [String]
                                              }
 
-data GallinaType = GallinaType { tyBoundVars :: [String] 
-                               , tyArgs :: [String] 
-                               , tyResult :: String
-                               }
+data GallinaType
+     = GallinaTyForall [String] GallinaType
+     | GallinaTyFun GallinaType GallinaType        
+     | GallinaTyVar String
+     | GallinaTyCon String
 
 data GallinaDefinition = GallinaDefinition { defName :: String
                                            , defType :: GallinaType
@@ -53,13 +54,36 @@ ppConstr n c  = constrName c ++ " : " ++ ( intercalate " -> " . (++ [n]) . field
 
 ppDefinition :: GallinaDefinition -> String
 ppDefinition d = "Definition " ++ defName d 
-                 ++ (if (not . null . tyBoundVars . defType $ d) 
-                    then " { " ++ (unwords . tyBoundVars . defType $ d) ++ " : Type }"
-                    else "")
-                 ++ " : " ++ intercalate " -> " t ++ " :=\n\t" ++ (ppTerm . defBody $ d) ++ "."
-  where t = tyArgs (defType d) ++ [tyResult (defType d)]
+                 ++ " " ++ (ppBoundVars . defType $ d)
+                 ++ " : " ++ (ppType . defType $ d) ++ " :=\n\t" ++ (ppTerm . defBody $ d) ++ "."
+
         
-testDefinition = GallinaDefinition "const" (GallinaType ["a", "b"] ["a", "b"] "a") undefined
+testDefinition :: GallinaDefinition
+testDefinition = GallinaDefinition "const" (GallinaTyForall ["a", "b"] (GallinaTyFun (GallinaTyVar "a") (GallinaTyFun (GallinaTyVar "b") (GallinaTyVar "a")))) undefined
+
+ppBoundVars :: GallinaType -> String
+ppBoundVars (GallinaTyForall l _) = if not (null l)
+                                    then "{ " ++ unwords l ++ " : Type }"
+                                    else ""
+ppBoundVars _ = ""
+
+-- Ignore foralls
+ppType :: GallinaType -> String
+ppType (GallinaTyForall _ t) = ppType t
+ppType (GallinaTyFun l r) = ppType l ++ " -> " ++ ppType r -- TODO: parentheses
+ppType (GallinaTyVar str) = str
+ppType (GallinaTyCon str) = str
 
 ppTerm :: GallinaTerm -> String
-ppTerm t = ""
+ppTerm _ = ""
+
+generalise :: GallinaType -> GallinaType
+generalise ty = let vars = ftv ty in if not (null vars) 
+                                     then GallinaTyForall vars ty
+                                     else ty
+
+ftv :: GallinaType -> [String]
+ftv (GallinaTyForall _ _) = error "ftv: foralls should not occur here"
+ftv (GallinaTyFun l r) = union (ftv l) (ftv r)
+ftv (GallinaTyVar str) = return str
+ftv (GallinaTyCon str) = return str
