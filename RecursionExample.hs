eea957b4b42d2abaa061015951378b468e2e089c
Gabe Dijkstra <gabe.dijkstra@gmail.com>
2012-09-28 14:45:17 +0200

    Recursive plus on nats and foldr on lists now work.

--- RecursionExample.hs
+++ RecursionExample.hs
@@ -1,19 +1,27 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
 module RecursionExample where
+
+plus :: Nat -> Nat -> Nat
+plus Zero x = x
+plus (Succ k) x = Succ (plus k x)
 
 data Nat = Zero
          | Succ Nat
 
--- idNat :: Nat -> Nat
--- idNat Zero = Zero
--- idNat (Succ k) = Succ (idNat k)
+foldr :: (a -> b -> b) -> b -> List a -> b
+foldr _ e Nil = e
+foldr f e (Cons x xs) = f x (foldr f e xs)
 
-a, b, c, d, e :: Iets
-a  = b
+data List a = Nil
+             | Cons a (List a)
+              
 
-b  = c
+data B = T | F
 
-c c = d
+not :: B -> B
+not T = id F
+not F = id T
 
-e = \b -> a
-
-d = e
+id :: a -> a
+id a = a
