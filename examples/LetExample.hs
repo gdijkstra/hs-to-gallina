543a4de8220d894624df746a038541adca6399a0
Gabe Dijkstra <gabe.dijkstra@gmail.com>
2012-10-12 14:41:32 +0200

    Added some more involved examples.

--- examples/LetExample.hs
+++ examples/LetExample.hs
@@ -1,4 +1,4 @@
-data Unit = Tt
+{-# LANGUAGE NoImplicitPrelude #-}
 
 data List a = Nil
             | Cons a (List a)
@@ -9,6 +9,8 @@ foldr k z = go
     go Nil         = z
     go (Cons y ys) = k y (go ys)
 
+data Unit = Tt
+
 letTest :: List Unit
 letTest = let unit = b
               b = Tt
@@ -17,4 +19,26 @@ letTest = let unit = b
     unit2 = unit4
     unit3 = unit4
     unit4 = Tt
+
+-- Example of dependency on List where the dependency comes from a
+-- constructor used in the "where" part.
+lengthTest :: Nat
+lengthTest = len test
+  where
+    len Nil = Zero
+    len (Cons _ xs) = Succ (len xs)
+    test = Cons Zero (Cons Zero (Cons Zero Nil))
+
+const :: a -> b -> a
+const a _ = a
+
+lengthTest' :: Nat
+lengthTest' = len test
+  where
+    len = foldr (const Succ) Zero
+    test = Cons Zero (Cons Zero (Cons Zero Nil))
+
+data Nat = Zero
+         | Succ Nat
+
 
