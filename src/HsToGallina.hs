e2d833e4aa6411977501fedc1d40a665ee91d61f
Gabe Dijkstra <gabe.dijkstra@gmail.com>
2012-11-16 12:03:30 +0100

    Changed handling of command line options.

--- src/HsToGallina.hs
+++ src/HsToGallina.hs
@@ -1,31 +1,69 @@
 import           AG
 import           BoveCapretta
 import           Data.Foldable          (forM_)
+import           Data.Maybe             (fromJust, fromMaybe, isNothing)
 import qualified Data.Set               as S
 import           Gallina.PrettyPrinting
 import           Gallina.Syntax
 import           Language.Haskell.Exts
+import           System.Console.GetOpt  (ArgDescr (..), ArgOrder (..),
+                                         OptDescr (..), getOpt, usageInfo)
 import           System.Environment     (getArgs)
 import           System.FilePath
 
 data Args =
-  Args { filePath  :: FilePath
-       , writePath :: Maybe FilePath
-       }
+  Args
+  { optShowVersion :: Bool
+  , optCoqOutput   :: Maybe FilePath
+  , optHsOutput    :: Maybe FilePath
+  , optInput       :: Maybe FilePath
+  }
+  deriving Show
 
-parseArgs :: [String] -> Maybe Args
-parseArgs [fp] = Just $ Args { filePath = fp, writePath = Nothing }
-parseArgs ["-w", fp] = Just $ Args { filePath = fp
-                                   , writePath = Just (replaceExtension fp ".v")
-                                   }
-parseArgs ["-w", outfp, fp] = Just $ Args { filePath = fp, writePath = Just outfp }
-parseArgs _ = Nothing
+defaultArgs :: Args
+defaultArgs = Args
+  { optShowVersion = False
+  , optCoqOutput   = Nothing
+  , optHsOutput    = Nothing
+  , optInput       = Nothing
+  }
 
-helpMessage :: IO ()
-helpMessage = do
-  putStrLn "HsToGallina [-w [OUTFILE]] FILE"
-  putStrLn "  -w: write the output to a file. If OUTFILE is not given, then"
-  putStrLn "      it will write to FILE with the extension replaced by \".v\""
+options :: [OptDescr (Args -> Args)]
+options =
+ [ Option ['V','?'] ["version"]
+   (NoArg (\opts -> opts { optShowVersion = True }))
+   "show version number"
+ , Option ['w'] ["write output to file"]
+   (OptArg ((\f opts -> opts { optCoqOutput = f }))
+    "FILE")
+   "output FILE"
+ , Option ['e'] ["extract"]
+   (OptArg ((\f opts -> opts { optHsOutput = f }))
+    "FILE")
+   "output FILE"
+ , Option ['f'] ["file"]
+   (ReqArg ((\f opts -> opts { optInput = Just f }))
+    "FILE")
+   "input FILE"
+ ]
+
+addCoqOutput :: Args -> Args
+addCoqOutput args = args { optCoqOutput = return . fromMaybe (replaceExtension inp ".v") $ cop }
+  where
+    inp = fromJust . optInput $ args
+    cop = optCoqOutput args
+
+parseArgs :: [String] -> Either String Args
+parseArgs argv = fmap addCoqOutput $
+   case getOpt Permute options argv of
+      (o, _, []  ) -> let res = foldl (flip id) defaultArgs o
+                    in if (isNothing . optInput $ res) && (not . optShowVersion $ res)
+                       then Left $ usage ["Error: HsToGallina requires -f FILE\n"]
+                       else Right res
+      (_, _, errs) -> Left $ usage errs
+  where
+    usage errs = concat errs ++ usageInfo header options
+    header = "Usage: HsToGallina -f FILE [OPTION...]"
 
 makeCoinductive :: Result -> Result
 makeCoinductive r = r { resVernacular = v { moduleDefinitions = newDefinitions } }
@@ -67,7 +105,7 @@ addImplicitArgumentsCommands r = r { resVernacular = v { moduleDefinitions = opt
 
 convertFile :: Args -> IO ()
 convertFile args = do
-  res <- parseFile . filePath $ args
+  res <- parseFile . fromJust . optInput $ args
   case res of
     ParseOk m -> do
       let output = ppVernacular
@@ -77,10 +115,10 @@ convertFile args = do
                    . applyBoveCapretta
                    . convertToGallina
                    $ m
-      forM_ (writePath args) (\outfp -> writeFile outfp output)
+      forM_ (optCoqOutput args) (\outfp -> writeFile outfp output)
     ParseFailed _ _ -> putStrLn "convertFile: Parsing Haskell file failed."
 
 main :: IO ()
 main = do
   as <- getArgs
-  maybe helpMessage convertFile (parseArgs as)
+  either putStrLn convertFile $ parseArgs as
