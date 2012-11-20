4ae51d804c81533ac3d3c305bbd9b5d6a66ff175
Gabe Dijkstra <gabe.dijkstra@gmail.com>
2012-11-20 15:22:29 +0100

    Added coinduction test to the test suite.

---
+++ tests/Coinduction.hs
@@ -0,0 +1,10 @@
+{-# OPTIONS_Hs2Gallina co: Stream zeroes #-}
+
+module Coinduction where
+
+data Stream a = Cons a (Stream a)
+
+data Nat = Zero | Succ Nat
+
+zeroes :: Stream Nat
+zeroes = Cons Zero zeroes
