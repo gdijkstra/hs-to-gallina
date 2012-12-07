59b9cb8a75ec981278ac8b90b4937b85ff26df93
Gabe Dijkstra <gabe.dijkstra@gmail.com>
2012-12-07 13:47:35 +0100

    Removed the "flip" stuff.

--- tests/Quicksort.hs
+++ tests/Quicksort.hs
@@ -3,14 +3,16 @@ module Quicksort where
 
 data Nat = Zero | Succ Nat
 
-lt :: Nat -> Nat -> Bool
-lt Zero     Zero     = False
-lt Zero     (Succ n) = True
-lt (Succ n) Zero     = False
-lt (Succ n) (Succ m) = lt n m
+-- x > y
+gt :: Nat -> Nat -> Bool
+gt Zero     Zero     = False
+gt Zero     (Succ n) = False
+gt (Succ n) Zero     = True
+gt (Succ n) (Succ m) = gt n m
 
-ge :: Nat -> Nat -> Bool
-ge a b = not (lt a b)
+-- x <= y
+le :: Nat -> Nat -> Bool
+le a b = not (gt a b)
 
 append :: [a] -> [a] -> [a]
 append [] ys       = ys
@@ -19,5 +21,5 @@ append (x : xs) ys = x : (append xs ys)
 quicksort :: [Nat] -> [Nat]
 quicksort []       = []
 quicksort (x : xs) = append
-                     (quicksort (filter ((flip lt) x) xs))
-                     (quicksort (filter ((flip ge) x) xs))
+                     (quicksort (filter (gt x) xs))
+                     (quicksort (filter (le x) xs))
