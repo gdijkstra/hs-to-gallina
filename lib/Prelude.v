42107072f564fce43e9323e2048245c15ed63fe6
Gabe Dijkstra <gabe.dijkstra@gmail.com>
2014-02-18 16:23:26 +0000

    Fixed faulty proof in Prelude.v by hand.

--- lib/Prelude.v
+++ lib/Prelude.v
@@ -40,10 +40,9 @@ Extract Inlined Constant otherwise => "Prelude.otherwise".
 
 (* Maybe *)
 
-Definition Maybe (a : Set) := option a.
-Definition Just := Some.
-Definition Nothing {a : Set} := None (A:=a).
-Arguments Just [A] _.
+Inductive Maybe (a : Set) : Set :=
+  | Just : a -> Maybe a
+  | Nothing : Maybe a.
 
 Definition maybe {a b : Set} (n : b) (f : a -> b) (m : Maybe a) : b :=
   match m with
@@ -51,10 +50,7 @@ Definition maybe {a b : Set} (n : b) (f : a -> b) (m : Maybe a) : b :=
     | Nothing => n
   end.
 
-Extract Constant Maybe "a" => "Prelude.Maybe a".
-Extract Inlined Constant Just => "Prelude.Just".
-Extract Inlined Constant Nothing => "Prelude.Nothing".
-Extract Inductive option => "Prelude.Maybe" [ "Prelude.Just" "Prelude.Nothing" ].
+Extract Inductive Maybe => "Prelude.Maybe" [ "Prelude.Just" "Prelude.Nothing" ].
 Extract Inlined Constant maybe => "Prelude.maybe".
 
 (* Either *)
@@ -64,8 +60,7 @@ Inductive Either (a b : Set) : Set :=
   | Right : b -> Either a b.  
 
 Arguments Left {a b} _.
-Arguments Right [a b] _.
-
+Arguments Right {a b} _.
 
 Definition either {a b c : Set} (f : a -> c) (g : b -> c) (e : Either a b) : c :=
   match e with
@@ -186,11 +181,11 @@ Definition or : list Bool -> Bool := foldr orb False.
 Definition any {a : Set} (p : a -> Bool) (xs : list a) : Bool := or (map a Bool p xs).
 Definition all {a : Set} (p : a -> Bool) (xs : list a) : Bool := and (map a Bool p xs).
 
-Arguments map [A B] _ _.
-Arguments filter [A] _ _.
-Arguments length [A] _.
-Arguments app [A] _ _.
-Arguments reverse [A] _.
+Arguments map {A B} _ _.
+Arguments filter {A} _ _.
+Arguments length {A} _.
+Arguments app {A} _ _.
+Arguments reverse {A} _.
 
 Extract Inlined Constant map => "Prelude.map".
 Extract Inlined Constant filter => "Prelude.filter".
@@ -217,113 +212,120 @@ Extract Inlined Constant all => "Prelude.all".
 
 (* Partial list functions *)
 
-(* Set Contextual Implicit. *)
-(* Set Implicit Arguments. *)
+Set Contextual Implicit.
+Set Implicit Arguments.
 
-(* Inductive head_acc ( a : Set ) : List a -> Prop := *)
-(*           | head_acc_0 : forall (x : a) (xs : List a) , head_acc (cons x xs). *)
+Inductive head_acc ( a : Set ) : List a -> Prop :=
+          | head_acc_0 : forall (x : a) (xs : List a) , head_acc (cons x xs).
 
-(* Inductive tail_acc ( a : Set ) : List a -> Prop := *)
-(*           | tail_acc_0 : forall (x : a) (xs : List a) , tail_acc (cons x xs). *)
+Inductive tail_acc ( a : Set ) : List a -> Prop :=
+          | tail_acc_0 : forall (x : a) (xs : List a) , tail_acc (cons x xs).
 
-(* Inductive last_acc ( a : Set ) : List a -> Prop := *)
-(*           | last_acc_0 : forall (x : a) , last_acc (cons x nil) *)
-(*           | last_acc_1 : forall (x : a) (y : a) (ys : List a) , last_acc ys -> last_acc (cons x (cons y ys)). *)
+Inductive last_acc ( a : Set ) : List a -> Prop :=
+          | last_acc_0 : forall (x : a) , last_acc (cons x nil)
+          | last_acc_1 : forall (x : a) (y : a) (ys : List a) , last_acc ys -> last_acc (cons x (cons y ys)).
 
-(* Inductive init_acc ( a : Set ) : List a -> Prop := *)
-(*           | init_acc_0 : forall (x : a) , init_acc (cons x nil) *)
-(*           | init_acc_1 : forall (x : a) (y : a) (ys : List a) , init_acc ys -> init_acc (cons x (cons y ys)). *)
+Inductive init_acc ( a : Set ) : List a -> Prop :=
+          | init_acc_0 : forall (x : a) , init_acc (cons x nil)
+          | init_acc_1 : forall (x : a) (y : a) (ys : List a) , init_acc ys -> init_acc (cons x (cons y ys)).
 
-(* Inductive foldr1_acc ( a : Set ) : (a -> a -> a) -> List a -> Prop := *)
-(*           | foldr1_acc_0 : forall (f : a -> a -> a) (x : a) , foldr1_acc f (cons x nil) *)
-(*           | foldr1_acc_1 : forall (f : a -> a -> a) (x : a) (y : a) (ys : List a) , foldr1_acc f (cons y ys) -> foldr1_acc f (cons x (cons y ys)). *)
+Inductive foldr1_acc ( a : Set ) : (a -> a -> a) -> List a -> Prop :=
+          | foldr1_acc_0 : forall (f : a -> a -> a) (x : a) , foldr1_acc f (cons x nil)
+          | foldr1_acc_1 : forall (f : a -> a -> a) (x : a) (y : a) (ys : List a) , foldr1_acc f (cons y ys) -> foldr1_acc f (cons x (cons y ys)).
 
-(* Inductive foldl1_acc ( a : Set ) : (a -> a -> a) -> List a -> Prop := *)
-(*           | foldl1_acc_0 : forall (f : a -> a -> a) (x : a) (xs : List a) , foldl1_acc f (cons x xs). *)
+Inductive foldl1_acc ( a : Set ) : (a -> a -> a) -> List a -> Prop :=
+          | foldl1_acc_0 : forall (f : a -> a -> a) (x : a) (xs : List a) , foldl1_acc f (cons x xs).
 
-(* (* Generated proof mess *) *)
+(* Generated proof mess *)
 
-(* Theorem head_acc_non_0 : forall (a : Set) (x0 : List a) , head_acc x0 -> (x0 = nil) -> Logic.False . *)
-(* intros a x0 H; case H; intros; discriminate. *)
-(* Defined. *)
+Theorem head_acc_non_0 : forall (a : Set) (x0 : List a) , head_acc x0 -> (x0 = nil) -> Logic.False .
+intros a x0 H; case H; intros; discriminate.
+Defined.
 
-(* Theorem last_acc_non_0 : forall (a : Set) (x0 : List a) , last_acc x0 -> (x0 = nil) -> Logic.False . *)
-(* intros a x0 H; case H; intros; discriminate. *)
-(* Defined. *)
+Theorem last_acc_non_0 : forall (a : Set) (x0 : List a) , last_acc x0 -> (x0 = nil) -> Logic.False .
+intros a x0 H; case H; intros; discriminate.
+Defined.
 
-(* Theorem last_acc_inv_1_0 : forall (a : Set) (x0 : List a) (x : a) (y : a) (ys : List a) , last_acc x0 -> (x0 = cons x (cons y ys)) -> last_acc ys . *)
-(* intros a x0 x y ys H; case H; try (intros; discriminate). intros x' y' ys' Hcall0 . intros Heq0; injection Heq0. intros Heq0_ctx_0 Heq0_ctx_1 Heq0_ctx_2. try (rewrite <- Heq0_ctx_0).  try (rewrite <- Heq0_ctx_1).  try (rewrite <- Heq0_ctx_2). assumption. *)
-(* Defined. *)
+Theorem last_acc_inv_1_0 : forall (a : Set) (x0 : List a) (x : a) (y : a) (ys : List a) , last_acc x0 -> (x0 = cons x (cons y ys)) -> last_acc ys .
+intros a x0 x y ys H; case H; try (intros; discriminate). intros x' y' ys' Hcall0 . intros Heq0; injection Heq0. intros Heq0_ctx_0 Heq0_ctx_1 Heq0_ctx_2. try (rewrite <- Heq0_ctx_0).  try (rewrite <- Heq0_ctx_1).  try (rewrite <- Heq0_ctx_2). assumption.
+Defined.
 
 
-(* Theorem tail_acc_non_0 : forall (a : Set) (x0 : List a) , tail_acc x0 -> (x0 = nil) -> Logic.False . *)
-(* intros a x0 H; case H; intros; discriminate. *)
-(* Defined. *)
+Theorem tail_acc_non_0 : forall (a : Set) (x0 : List a) , tail_acc x0 -> (x0 = nil) -> Logic.False .
+intros a x0 H; case H; intros; discriminate.
+Defined.
 
-(* Theorem init_acc_non_0 : forall (a : Set) (x0 : List a) , init_acc x0 -> (x0 = nil) -> Logic.False . *)
-(* intros a x0 H; case H; intros; discriminate. *)
-(* Defined. *)
+Theorem init_acc_non_0 : forall (a : Set) (x0 : List a) , init_acc x0 -> (x0 = nil) -> Logic.False .
+intros a x0 H; case H; intros; discriminate.
+Defined.
 
-(* Theorem init_acc_inv_1_0 : forall (a : Set) (x0 : List a) (x : a) (y : a) (ys : List a) , init_acc x0 -> (x0 = cons x (cons y ys)) -> init_acc ys . *)
-(* intros a x0 x y ys H; case H; try (intros; discriminate). intros x' y' ys' Hcall0 . intros Heq0; injection Heq0. intros Heq0_ctx_0 Heq0_ctx_1 Heq0_ctx_2. try (rewrite <- Heq0_ctx_0).  try (rewrite <- Heq0_ctx_1).  try (rewrite <- Heq0_ctx_2). assumption. *)
-(* Defined. *)
+Theorem init_acc_inv_1_0 : forall (a : Set) (x0 : List a) (x : a) (y : a) (ys : List a) , init_acc x0 -> (x0 = cons x (cons y ys)) -> init_acc ys .
+intros a x0 x y ys H; case H; try (intros; discriminate). intros x' y' ys' Hcall0 . intros Heq0; injection Heq0. intros Heq0_ctx_0 Heq0_ctx_1 Heq0_ctx_2. try (rewrite <- Heq0_ctx_0).  try (rewrite <- Heq0_ctx_1).  try (rewrite <- Heq0_ctx_2). assumption.
+Defined.
 
-(* Theorem foldr1_acc_non_0 : forall (a : Set) (x0 : a -> a -> a) (x1 : List a) (_q0 : a -> a -> a) , foldr1_acc x0 x1 -> (x0 = _q0) -> (x1 = nil) -> Logic.False . *)
-(* intros a x0 x1 _q0 H; case H; intros; discriminate. *)
-(* Defined. *)
+Theorem foldr1_acc_non_0 : forall (a : Set) (x0 : a -> a -> a) (x1 : List a) (_q0 : a -> a -> a) , foldr1_acc x0 x1 -> (x0 = _q0) -> (x1 = nil) -> Logic.False .
+intros a x0 x1 _q0 H; case H; intros; discriminate.
+Defined.
 
-(* Theorem foldr1_acc_inv_1_0 : forall (a : Set) (x0 : a -> a -> a) (x1 : List a) (f : a -> a -> a) (x : a) (y : a) (ys : List a) , foldr1_acc x0 x1 -> (x0 = f) -> (x1 = cons x (cons y ys)) -> foldr1_acc f (cons y ys) . *)
-(* intros a x0 x1 f x y ys H; case H; try (intros; discriminate). intros f' x' y' ys' Hcall0 . intros Heq0; injection Heq0. intros Heq0_ctx_0. try (rewrite <- Heq0_ctx_0). intros Heq1; injection Heq1. intros Heq1_ctx_0 Heq1_ctx_1 Heq1_ctx_2. try (rewrite <- Heq1_ctx_0).  try (rewrite <- Heq1_ctx_1).  try (rewrite <- Heq1_ctx_2). assumption. *)
-(* Defined. *)
+Theorem foldr1_acc_inv_1_0 : 
+  forall (a : Set) 
+         (x0 : a -> a -> a)
+         (x1 : List a)
+         (f : a -> a -> a)
+         (x : a) (y : a)
+         (ys : List a) , foldr1_acc x0 x1 -> (x0 = f) -> (x1 = cons x (cons y ys)) -> foldr1_acc f (cons y ys) .
+intros a x0 x1 f x y ys H; case H; try (intros; discriminate). 
+intros f' x' y' ys' Hcall0. intro Heq0; try (rewrite <- Heq0). intros Heq1; injection Heq1. intros Heq1_ctx_0 Heq1_ctx_1 Heq1_ctx_2. try (rewrite <- Heq1_ctx_0).  try (rewrite <- Heq1_ctx_1).  try (rewrite <- Heq1_ctx_2). assumption.
+Defined.
 
-(* Theorem foldl1_acc_non_0 : forall (a : Set) (x0 : a -> a -> a) (x1 : List a) (_q0 : a -> a -> a) , foldl1_acc x0 x1 -> (x0 = _q0) -> (x1 = nil) -> Logic.False . *)
-(* intros a x0 x1 _q0 H; case H; intros; discriminate. *)
-(* Defined. *)
+Theorem foldl1_acc_non_0 : forall (a : Set) (x0 : a -> a -> a) (x1 : List a) (_q0 : a -> a -> a) , foldl1_acc x0 x1 -> (x0 = _q0) -> (x1 = nil) -> Logic.False .
+intros a x0 x1 _q0 H; case H; intros; discriminate.
+Defined.
 
-(* Unset Implicit Arguments. *)
+Unset Implicit Arguments.
 
-(* Definition head {a : Set} (x0 : List a) (x1 : head_acc x0) : a := *)
-(*              match x0 as _y0 return (x0 = _y0) -> a with *)
-(*                | cons x xs => fun _h0 => x *)
-(*                | nil => fun _h0 => False_rec a (head_acc_non_0 x1 _h0) *)
-(*              end (refl_equal x0). *)
+Definition head {a : Set} (x0 : List a) (x1 : head_acc x0) : a :=
+             match x0 as _y0 return (x0 = _y0) -> a with
+               | cons x xs => fun _h0 => x
+               | nil => fun _h0 => False_rec a (head_acc_non_0 x1 _h0)
+             end (refl_equal x0).
 
-(* Definition tail {a : Set} (x0 : List a) (x1 : tail_acc x0) : List a := *)
-(*              match x0 as _y0 return (x0 = _y0) -> List a with *)
-(*                | cons x xs => fun _h0 => xs *)
-(*                | nil => fun _h0 => False_rec (List a) (tail_acc_non_0 x1 _h0) *)
-(*              end (refl_equal x0). *)
+Definition tail {a : Set} (x0 : List a) (x1 : tail_acc x0) : List a :=
+             match x0 as _y0 return (x0 = _y0) -> List a with
+               | cons x xs => fun _h0 => xs
+               | nil => fun _h0 => False_rec (List a) (tail_acc_non_0 x1 _h0)
+             end (refl_equal x0).
 
-(* Fixpoint last { a : Set } (x0 : List a) (x1 : last_acc x0) : a := *)
-(*            match x0 as _y0 return (x0 = _y0) -> a with *)
-(*              | cons x nil => fun _h0 => x *)
-(*              | cons x (cons y ys) => fun _h0 => last ys (last_acc_inv_1_0 x1 _h0) *)
-(*              | nil => fun _h0 => False_rec a (last_acc_non_0 x1 _h0) *)
-(*            end (refl_equal x0). *)
+Fixpoint last { a : Set } (x0 : List a) (x1 : last_acc x0) : a :=
+           match x0 as _y0 return (x0 = _y0) -> a with
+             | cons x nil => fun _h0 => x
+             | cons x (cons y ys) => fun _h0 => last ys (last_acc_inv_1_0 x1 _h0)
+             | nil => fun _h0 => False_rec a (last_acc_non_0 x1 _h0)
+           end (refl_equal x0).
 
-(* Fixpoint init { a : Set } (x0 : List a) (x1 : init_acc x0) : List a := *)
-(*            match x0 as _y0 return (x0 = _y0) -> List a with *)
-(*              | cons x nil => fun _h0 => nil *)
-(*              | cons x (cons y ys) => fun _h0 => cons x (cons y (init ys (init_acc_inv_1_0 x1 _h0))) *)
-(*              | nil => fun _h0 => False_rec (List a) (init_acc_non_0 x1 _h0) *)
-(*            end (refl_equal x0). *)
+Fixpoint init { a : Set } (x0 : List a) (x1 : init_acc x0) : List a :=
+           match x0 as _y0 return (x0 = _y0) -> List a with
+             | cons x nil => fun _h0 => nil
+             | cons x (cons y ys) => fun _h0 => cons x (cons y (init ys (init_acc_inv_1_0 x1 _h0)))
+             | nil => fun _h0 => False_rec (List a) (init_acc_non_0 x1 _h0)
+           end (refl_equal x0).
 
-(* Fixpoint foldr1 { a : Set } (x0 : a -> a -> a) (x1 : List a) (x2 : foldr1_acc x0 x1) : a := *)
-(*            match x0 as _y0, x1 as _y1 return (x0 = _y0) -> (x1 = _y1) -> a with *)
-(*              | f, cons x nil => fun _h0 _h1 => x *)
-(*              | f, cons x (cons y ys) => fun _h0 _h1 => f x (foldr1 f (cons y ys) (foldr1_acc_inv_1_0 x2 _h0 _h1)) *)
-(*              | _q0, nil => fun _h0 _h1 => False_rec a (foldr1_acc_non_0 x2 _h0 _h1) *)
-(*            end (refl_equal x0) (refl_equal x1). *)
+Fixpoint foldr1 { a : Set } (x0 : a -> a -> a) (x1 : List a) (x2 : foldr1_acc x0 x1) : a :=
+           match x0 as _y0, x1 as _y1 return (x0 = _y0) -> (x1 = _y1) -> a with
+             | f, cons x nil => fun _h0 _h1 => x
+             | f, cons x (cons y ys) => fun _h0 _h1 => f x (foldr1 f (cons y ys) (foldr1_acc_inv_1_0 x2 _h0 _h1))
+             | _q0, nil => fun _h0 _h1 => False_rec a (foldr1_acc_non_0 x2 _h0 _h1)
+           end (refl_equal x0) (refl_equal x1).
 
-(* Definition foldl1 {a : Set} (x0 : a -> a -> a) (x1 : List a) (x2 : foldl1_acc x0 x1) : a := *)
-(*              match x0 as _y0, x1 as _y1 return (x0 = _y0) -> (x1 = _y1) -> a with *)
-(*                | f, cons x xs => fun _h0 _h1 => foldl f x xs *)
-(*                | _q0, nil => fun _h0 _h1 => False_rec a (foldl1_acc_non_0 x2 _h0 _h1) *)
-(*              end (refl_equal x0) (refl_equal x1). *)
+Definition foldl1 {a : Set} (x0 : a -> a -> a) (x1 : List a) (x2 : foldl1_acc x0 x1) : a :=
+             match x0 as _y0, x1 as _y1 return (x0 = _y0) -> (x1 = _y1) -> a with
+               | f, cons x xs => fun _h0 _h1 => foldl f x xs
+               | _q0, nil => fun _h0 _h1 => False_rec a (foldl1_acc_non_0 x2 _h0 _h1)
+             end (refl_equal x0) (refl_equal x1).
 
-(* Extract Inlined Constant head => "Prelude.head". *)
-(* Extract Inlined Constant tail => "Prelude.tail". *)
-(* Extract Inlined Constant last => "Prelude.last". *)
-(* Extract Inlined Constant init => "Prelude.init". *)
-(* Extract Inlined Constant foldr1 => "Prelude.foldr1". *)
-(* Extract Inlined Constant foldl1 => "Prelude.foldl1". *)
+Extract Inlined Constant head => "Prelude.head".
+Extract Inlined Constant tail => "Prelude.tail".
+Extract Inlined Constant last => "Prelude.last".
+Extract Inlined Constant init => "Prelude.init".
+Extract Inlined Constant foldr1 => "Prelude.foldr1".
+Extract Inlined Constant foldl1 => "Prelude.foldl1".
